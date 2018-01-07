6350ad6ae03cbd4f093523d4be61d8ed9f4ecc87
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2018-01-06 17:58:16 -0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+//Bowen Johnson G01597652 Project2
+
+#include "myutil.h"
+#include "db.h"
+#include "song.h"
+
+
+int main() {
+
+	songidx sidx;
+
+	sidx.assignarray(sidx);
+	sidx.selection(sidx);
+
+	return 0;
+}
