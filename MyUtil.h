25919f0356e7fa1e44a7ee8bd2a5156c65cfc13c
Bowen <34555759+shiggitymiggity@users.noreply.github.com>
2018-01-06 18:32:18 -0800

    Create MyUtil.h

---
+++ MyUtil.h
@@ -0,0 +1,19 @@
+#ifndef MYUTIL_H
+#define MYUTIL_H
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+void displaymenu();
+
+int charcntr(char line[]);
+
+
+#endif
